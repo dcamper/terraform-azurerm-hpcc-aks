d2253c0feaa932a6abc9d3157a9e5821f54c4b41
Dan S. Camper <dan.camper@lexisnexisrisk.com>
2021-09-22 12:29:36 -0500

    Outputs for stopping/starting AKS cluster
    Signed-off-by: Dan S. Camper <dan.camper@lexisnexisrisk.com>

--- output.tf
+++ output.tf
@@ -9,3 +9,11 @@ output "aks_login" {
 output "rsg_name" {
   value = "Resource group for created items: ${module.resource_group.name}"
 }
+
+output "stop_cluster_cmd" {
+  value = "To stop the AKS cluster: az aks stop --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
+}
+
+output "start_cluster_cmd" {
+  value = "To restart the AKS cluster: az aks start --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
+}
