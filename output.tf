8b709d345db977ca3919eee55fd1a101ae6d7884
Dan S. Camper <dan.camper@lexisnexisrisk.com>
2021-09-23 06:54:54 -0500

    Shorten output values
    Signed-off-by: Dan S. Camper <dan.camper@lexisnexisrisk.com>

--- output.tf
+++ output.tf
@@ -6,14 +6,14 @@ output "aks_login" {
   value = "az aks get-credentials --name ${module.kubernetes.name} --resource-group ${module.resource_group.name} --admin"
 }
 
-output "rsg_name" {
-  value = "Resource group for created items: ${module.resource_group.name}"
+output "resource_group_name" {
+  value = "${module.resource_group.name}"
 }
 
 output "stop_cluster_cmd" {
-  value = "To stop the AKS cluster: az aks stop --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
+  value = "az aks stop --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
 }
 
 output "start_cluster_cmd" {
-  value = "To restart the AKS cluster: az aks start --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
+  value = "az aks start --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
 }
