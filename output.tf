e1254ce0668e4b9efa5fa393bc2bceb08c28f1d7
Ubuntu <azureuser@wipe-play.pkn05rdko4jejloub4w2s5lh3e.cx.internal.cloudapp.net>
2023-03-10 15:06:44 +0000

    Storaged original outputs and added 2 new outputs.

--- output.tf
+++ output.tf
@@ -3,8 +3,36 @@ output "url" {
   value       = format("http://%s.%s:8010",var.a_record_name, module.child_dns_zone.name)
 
   depends_on  = [module.child_dns_zone]
-} 
+}
 
 output "eclwatchip" {
   value       = data.external.get_eclwatchip.result["ecl_watch_ip"]
-} 
+}
+
+output "advisor_recommendations" {
+  value = data.azurerm_advisor_recommendations.advisor.recommendations
+}
+
+output "aks_login" {
+  value = "az aks get-credentials --name ${module.kubernetes.name} --resource-group ${module.resource_group.name} --admin"
+}
+
+output "resource_group_name" {
+  value = "${module.resource_group.name}"
+}
+
+output "stop_cluster_cmd" {
+  value = "az aks stop --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
+}
+
+output "start_cluster_cmd" {
+  value = "az aks start --name ${module.kubernetes.name} --resource-group ${module.resource_group.name}"
+}
+
+output "azure_region" {
+  value = module.resource_group.location
+}
+
+output "subscription_id" {
+  value = data.azurerm_subscription.current.subscription_id
+}
