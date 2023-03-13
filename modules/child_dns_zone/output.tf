31a1d5d646a2291da122f6b71e549db1310cc7fe
Ubuntu <azureuser@wipe-play.pkn05rdko4jejloub4w2s5lh3e.cx.internal.cloudapp.net>
2023-03-13 17:22:51 +0000

    Did chmod 755 modules/child_dns_zone/scripts/current_subscription_info.pl

--- modules/child_dns_zone/output.tf
+++ modules/child_dns_zone/output.tf
@@ -1,7 +1,7 @@
 output "name" {
-  value       = data.external.child_dns_zone[0].result["child_dns_name"]
+  value       = (var.dns_zone_name_prefix != "")? data.external.child_dns_zone[0].result["child_dns_name"] : null
 } 
 
 output "dns_resource_group" {
-  value       = data.external.child_dns_zone[0].result["child_dns_resource_group"]
+  value       = (var.dns_zone_name_prefix != "")? data.external.child_dns_zone[0].result["child_dns_resource_group"] : null
 } 
